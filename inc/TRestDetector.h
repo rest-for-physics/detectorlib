d5e4b277a86737d262cf693482ad69410944ea8d
j.a.galan.81@gmail.com <javier.galan@unizar.es>
2021-04-08 11:12:28 +0200

    Adding documentation. Removed fDriftAmplification.

--- inc/TRestDetector.h
+++ inc/TRestDetector.h
@@ -1,16 +1,24 @@
-////////////////////////////////////////////////////////////////////////////////////
-///
-///
-///             RESTSoft : Software for Rare Event Searches with TPCs
-///
-///             TRestDetector.h
-///
-///             Metadata class to be used to store basic detector setup info
-///             inherited from TRestMetadata
-///
-///             jun 2016:   First concept. Javier Galan
-///
-////////////////////////////////////////////////////////////////////////////////////
+/*************************************************************************
+ * This file is part of the REST software framework.                     *
+ *                                                                       *
+ * Copyright (C) 2016 GIFNA/TREX (University of Zaragoza)                *
+ * For more information see http://gifna.unizar.es/trex                  *
+ *                                                                       *
+ * REST is free software: you can redistribute it and/or modify          *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * REST is distributed in the hope that it will be useful,               *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have a copy of the GNU General Public License along with   *
+ * REST in $REST_PATH/LICENSE.                                           *
+ * If not, see http://www.gnu.org/licenses/.                             *
+ * For the list of contributors see $REST_PATH/CREDITS.                  *
+ *************************************************************************/
 
 #ifndef RestCore_TRestDetector
 #define RestCore_TRestDetector
@@ -34,33 +42,56 @@
 
 using namespace std;
 
+//! An metadata class to store basic detector information
 class TRestDetector : public TRestMetadata {
    public:
+    /// The detector name
     string fDetectorName = "REST Detector";
 
-    Double_t fDriftVoltage = -1;   // In V
-    Double_t fDriftDistance = -1;  // in mm
-    Double_t fDriftGain = -1;      // in mm
+    /// The detector drift voltage in V
+    Double_t fDriftVoltage = -1;
+    /// The detector drift distance in mm
+    Double_t fDriftDistance = -1;
+    /// The detector amplification voltage in V
+    Double_t fAmplificationVoltage = -1;
+    /// The detector amplification distance in mm (also known as amplificationGap)
+    Double_t fAmplificationDistance = -1;
+    /// The detector amplification gain
+    Double_t fAmplificationGain = -1;
 
-    Double_t fAmplificationVoltage = -1;   // In V
-    Double_t fAmplificationDistance = -1;  // in mm
-    Double_t fAmplificationGain = -1;      //
+    /// The detector active mass in kg
+    Double_t fMass = -1;
+    /// The detector active volume height in mm
+    Double_t fHeight = -1;
+    /// The detector active volume radius in mm
+    Double_t fRadius = -1;
+    /// The detector active volume length in mm
+    Double_t fLength = -1;
+    /// The detector active volume width in mm
+    Double_t fWidth = -1;
+    /// The detector active volume pressure in atm
+    Double_t fPressure = -1;
 
-    Double_t fMass = -1;    // in kg
-    Double_t fHeight = -1;  // in mm
-    Double_t fRadius = -1;  // in mm
-    Double_t fLength = -1;  // in mm
-    Double_t fWidth = -1;   // in mm
-    Double_t fPressure = -1; // in atm
+    /// The electronics shaping time in us
+    Double_t fElectronicsShapingTime = -1;
 
-    Double_t fDAQShapingTime = -1;   // in us
-    Double_t fDAQSamplingTime = -1;  // in us
-    Double_t fDAQDynamicRange = -1;
-    Double_t fDAQThreshold = -1;
-    
-    string fElectronicsGain;    //hexadecimal
-    string fElectronicsClock;   //hexadecimal
-    string fElectronicsShaping; //hexadecimal
+    /// The electronics sampling time in us
+    Double_t fElectronicsSamplingTime = -1;
+
+    /// The electronics dynamic range in V
+    Double_t fElectronicsDynamicRange = -1;
+
+    /// The electronics threshold in ADC units
+    Double_t fElectronicsThreshold = -1;
+
+    /// The electronics gain in raw configuration format (hexadecimal)
+    string fElectronicsGain = "-1";
+
+    /// The electronics clock (sampling) in raw configuration format (hexadecimal)
+    string fElectronicsClock = "-1";
+
+    /// The electronics shaping in raw configuration format (hexadecimal)
+    string fElectronicsShaping = "-1";
 
     void InitFromConfigFile() { ReadAllParameters(); }
     void PrintMetadata();
--- src/TRestDetector.cxx
+++ src/TRestDetector.cxx
@@ -1,3 +1,58 @@
+/*************************************************************************
+ * This file is part of the REST software framework.                     *
+ *                                                                       *
+ * Copyright (C) 2016 GIFNA/TREX (University of Zaragoza)                *
+ * For more information see http://gifna.unizar.es/trex                  *
+ *                                                                       *
+ * REST is free software: you can redistribute it and/or modify          *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * REST is distributed in the hope that it will be useful,               *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have a copy of the GNU General Public License along with   *
+ * REST in $REST_PATH/LICENSE.                                           *
+ * If not, see http://www.gnu.org/licenses/.                             *
+ * For the list of contributors see $REST_PATH/CREDITS.                  *
+ *************************************************************************/
+
+//////////////////////////////////////////////////////////////////////////
+///
+/// This class is intended to serve as storage of basic detector properties. Basic members
+/// exist with common information to most of the detector setups. More specialized detector
+/// setups might inherit from this class to extend the detector description.
+///
+/// <hr>
+///
+/// \warning **⚠ REST is under continous development.** This
+/// documentation /// is offered to you by the REST community. Your HELP is needed to keep this
+/// code up to date. Your feedback will be worth to support this software, please
+/// report any problems/suggestions you may find will using it at [The REST Framework
+/// forum](http://ezpc10.unizar.es). You are welcome to contribute fixing typos,
+/// updating information or adding/proposing new contributions. See also our
+/// <a href="https://github.com/rest-for-physics/framework/blob/master/CONTRIBUTING.md">Contribution
+/// Guide</a>.
+///
+///
+///_______________________________________________________________________________
+///
+/// RESTsoft - Software for Rare Event Searches with TPCs
+///
+/// History of developments:
+///
+/// 2021-April. First implementation
+///
+/// \class      TRestDetector
+/// \author     Javier Galan
+/// \author     Kaixiang Ni
+/// \author     David Diez
+///
+/// <hr>
+///
 #include "TRestDetector.h"
 
 void TRestDetector::PrintMetadata() {
