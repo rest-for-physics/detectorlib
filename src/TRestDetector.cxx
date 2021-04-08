d4eaec733bde9536842ab0489a148471fb27b1d1
j.a.galan.81@gmail.com <javier.galan@unizar.es>
2021-04-08 11:19:24 +0200

    TRestDetector::PrintMetadata. Simplified output

--- src/TRestDetector.cxx
+++ src/TRestDetector.cxx
@@ -61,8 +61,7 @@ void TRestDetector::PrintMetadata() {
     for (int i = 0; i < cl.GetNumberOfDataMembers(); i++) {
         if (cl.GetDataMember(i).name == "fgIsA") continue;
         if (cl.GetDataMember(i).ToString() != "-1")
-            metadata << "Field: " << cl.GetDataMember(i).name << ", value: " << cl.GetDataMember(i).ToString()
-                     << endl;
+            metadata << cl.GetDataMember(i).name << ": " << cl.GetDataMember(i).ToString() << endl;
     }
     metadata << "---------------------------------------" << endl;
 }
