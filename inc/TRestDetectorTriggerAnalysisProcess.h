193318217849555929f6f73fc4234bb2eadb1522
nkx <bur_ning@outlook.com>
2021-08-11 20:28:14 +0800

    fSampling and fADCLength added initial value

--- inc/TRestDetectorTriggerAnalysisProcess.h
+++ inc/TRestDetectorTriggerAnalysisProcess.h
@@ -45,10 +45,10 @@ class TRestDetectorTriggerAnalysisProcess : public TRestEventProcess {
 
    protected:
     /// It defines the sampling time in us to treat the signal as if it would be a rawsignal.
-    Double_t fSampling;
+    Double_t fSampling = 0.2;
 
     /// It defines the number of bins used to define the sampling depth.
-    Int_t fADCLength;
+    Int_t fADCLength = 512;
 
    public:
     /// Returns a pointer to the input signal event
