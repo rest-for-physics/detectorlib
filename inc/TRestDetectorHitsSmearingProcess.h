0f6bfecb4e355edcf445c12c5e21cba028f3b0d7
Javier Galan <javier.galan@unizar.es>
2023-03-02 15:16:11 +0100

    TRestDetectorHitsSmearingProcess. Adding documentation

--- inc/TRestDetectorHitsSmearingProcess.h
+++ inc/TRestDetectorHitsSmearingProcess.h
@@ -1,17 +1,24 @@
-///______________________________________________________________________________
-///______________________________________________________________________________
-///______________________________________________________________________________
-///
-///
-///             RESTSoft : Software for Rare Event Searches with TPCs
-///
-///             TRestDetectorHitsSmearingProcess.h
-///
-///             feb 2016:   First concept
-///                 Created as part of the conceptualization of existing REST
-///                 software.
-///                 Javier G. Garza
-///_______________________________________________________________________________
+/*************************************************************************
+ * This file is part of the REST software framework.                     *
+ *                                                                       *
+ * Copyright (C) 2016 GIFNA/TREX (University of Zaragoza)                *
+ * For more information see https://gifna.unizar.es/trex                 *
+ *                                                                       *
+ * REST is free software: you can redistribute it and/or modify          *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * REST is distributed in the hope that it will be useful,               *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have a copy of the GNU General Public License along with   *
+ * REST in $REST_PATH/LICENSE.                                           *
+ * If not, see https://www.gnu.org/licenses/.                            *
+ * For the list of contributors see $REST_PATH/CREDITS.                  *
+ *************************************************************************/
 
 #ifndef RestCore_TRestDetectorHitsSmearingProcess
 #define RestCore_TRestDetectorHitsSmearingProcess
@@ -19,9 +26,9 @@
 #include <TRandom3.h>
 #include <TRestEventProcess.h>
 
-#include "TRestDetectorGas.h"
 #include "TRestDetectorHitsEvent.h"
 
+/// A process to include detector energy resolution in a TRestDetectorHitsEvent
 class TRestDetectorHitsSmearingProcess : public TRestEventProcess {
    private:
     TRestDetectorHitsEvent* fHitsInputEvent;   //!
@@ -29,23 +36,21 @@ class TRestDetectorHitsSmearingProcess : public TRestEventProcess {
 
     TRandom3* fRandom;  //!
 
-    TRestDetectorGas* fGas;  //!
-
     void InitFromConfigFile() override;
     void Initialize() override;
     void LoadDefaultConfig();
 
    protected:
-    // add here the members of your event process
+    /// Reference energy for the FWHM
+    Double_t fEnergyRef = 5.9;  //<
 
-    Double_t fEnergyRef;         ///< reference energy for the FWHM
-    Double_t fResolutionAtERef;  ///< FWHM at Energy of reference
+    /// FWHM at Energy of reference
+    Double_t fResolutionAtERef = 15;  //<
 
    public:
     any GetInputEvent() const override { return fHitsInputEvent; }
     any GetOutputEvent() const override { return fHitsOutputEvent; }
 
-    void InitProcess() override;
     TRestEvent* ProcessEvent(TRestEvent* inputEvent) override;
     void EndProcess() override;
 
--- src/TRestDetectorHitsSmearingProcess.cxx
+++ src/TRestDetectorHitsSmearingProcess.cxx
@@ -1,60 +1,95 @@
-///______________________________________________________________________________
-///______________________________________________________________________________
-///______________________________________________________________________________
-///
-///
-///             RESTSoft : Software for Rare Event Searches with TPCs
-///
-///             TRestDetectorHitsSmearingProcess.cxx
-///
-///             Template to use to design "event process" classes inherited from
-///             TRestDetectorHitsSmearingProcess
-///             How to use: replace TRestDetectorHitsSmearingProcess by your name,
-///             fill the required functions following instructions and add all
-///             needed additional members and funcionality
-///
-///             feb 2016:   First concept
-///                 Created as part of the conceptualization of existing REST
-///                 software.
-///                 Javier G. Garza
-///_______________________________________________________________________________
+/*************************************************************************
+ * This file is part of the REST software framework.                     *
+ *                                                                       *
+ * Copyright (C) 2016 GIFNA/TREX (University of Zaragoza)                *
+ * For more information see http://gifna.unizar.es/trex                  *
+ *                                                                       *
+ * REST is free software: you can redistribute it and/or modify          *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * REST is distributed in the hope that it will be useful,               *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have a copy of the GNU General Public License along with   *
+ * REST in $REST_PATH/LICENSE.                                           *
+ * If not, see http://www.gnu.org/licenses/.                             *
+ * For the list of contributors see $REST_PATH/CREDITS.                  *
+ *************************************************************************/
 
+//////////////////////////////////////////////////////////////////////////
+/// TRestDetectorHitsSmearingProcess will introduce a stochastic smearing on the
+/// energy of the hits for each event. The total energy of each event will be
+/// re-distributed following a gaussian shaped function.
+///
+/// The energy resolution will follow a root square law with the energy
+/// using the `energyReference` and the `resolutionReference` as a pivoting
+/// point.
+///
+/// Therefore, the process defines only two parameters, one being the energy
+/// at wich the energy resolution is "known", and the FWHM in percentage at
+/// that given energy, `resolutionReference`.
+///
+/// \code
+///    <addProcess type="TRestDetectorHitsSmearingProcess" name="smear"
+///                title="Energy resolution smearing">
+///        <parameter name="energyReference" value="250keV" />
+///        <parameter name="resolutionReference" value="15" />
+///    </addProcess>
+/// \endcode
+///
+/// The following image is the result of smearing the energy of an electron
+/// gun of 250keV. It was produced using the exercise 3.2 inside the
+/// [day3/session2](https://github.com/rest-for-physics/rest-school/tree/master/day3/session2)
+/// at the rest-school repository.
+///
+/// \htmlonly <style>div.image img[src="smear.png"]{width:500px;}</style> \endhtmlonly
+/// ![The result of applying a 15% energy resolution at 250keV](smear.png)
+///
+///--------------------------------------------------------------------------
+///
+/// RESTsoft - Software for Rare Event Searches with TPCs
+///
+/// History of developments:
+///
+/// 2016-February: First implementation of TRestDetectorHitsSmearingProcess
+/// \author Javier G. Garza
+///
+/// 2023-January: This class has been documented
+/// \author Javier Galan - javier.galan@unizar.es
+///
+/// \class TRestDetectorHitsSmearingProcess
+///
+/// <hr>
+///
 #include "TRestDetectorHitsSmearingProcess.h"
 
 using namespace std;
 
 ClassImp(TRestDetectorHitsSmearingProcess);
 
+///////////////////////////////////////////////
+/// \brief Default constructor
+///
 TRestDetectorHitsSmearingProcess::TRestDetectorHitsSmearingProcess() { Initialize(); }
 
-TRestDetectorHitsSmearingProcess::TRestDetectorHitsSmearingProcess(const char* configFilename) {
-    Initialize();
-
-    if (LoadConfigFromFile(configFilename)) {
-        LoadDefaultConfig();
-    }
-
-    PrintMetadata();
-}
-
+///////////////////////////////////////////////
+/// \brief Default destructor
+///
 TRestDetectorHitsSmearingProcess::~TRestDetectorHitsSmearingProcess() {
     delete fHitsOutputEvent;
     // TRestDetectorHitsSmearingProcess destructor
 }
 
-void TRestDetectorHitsSmearingProcess::LoadDefaultConfig() {
-    SetTitle("Default config");
-
-    fEnergyRef = 5.9;
-    fResolutionAtERef = 15.0;
-}
-
+///////////////////////////////////////////////
+/// \brief Function to initialize input/output event members and define the section name
+///
 void TRestDetectorHitsSmearingProcess::Initialize() {
     SetSectionName(this->ClassName());
     SetLibraryVersion(LIBRARY_VERSION);
-
-    fEnergyRef = 5.9;
-    fResolutionAtERef = 15.0;
 
     fHitsInputEvent = nullptr;
     fHitsOutputEvent = new TRestDetectorHitsEvent();
@@ -62,25 +97,9 @@ void TRestDetectorHitsSmearingProcess::Initialize() {
     fRandom = nullptr;
 }
 
-void TRestDetectorHitsSmearingProcess::LoadConfig(const string& configFilename, const string& name) {
-    if (LoadConfigFromFile(configFilename, name)) {
-        LoadDefaultConfig();
-    }
-
-    PrintMetadata();
-
-    fGas = GetMetadata<TRestDetectorGas>();
-}
-
-void TRestDetectorHitsSmearingProcess::InitProcess() {
-    // Function to be executed once at the beginning of process
-    // (before starting the process of the events)
-
-    // Start by calling the InitProcess function of the abstract class.
-    // Comment this if you don't want it.
-    // TRestEventProcess::InitProcess();
-}
-
+///////////////////////////////////////////////
+/// \brief The main processing event function
+///
 TRestEvent* TRestDetectorHitsSmearingProcess::ProcessEvent(TRestEvent* inputEvent) {
     fHitsInputEvent = (TRestDetectorHitsEvent*)inputEvent;
     fHitsOutputEvent->SetEventInfo(fHitsInputEvent);
