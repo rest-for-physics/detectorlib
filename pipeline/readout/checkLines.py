ac279f8888b570b01d7243e341d5094f2b8b515e
Javier Galan <javier.galan@unizar.es>
2023-06-05 14:53:00 +0200

    Updating checkLines validation

---
+++ pipeline/readout/checkLines.py
@@ -0,0 +1,9 @@
+with open(r"output.txt", 'r') as fp:
+    x = len(fp.readlines())
+
+    if( x > 12 ):
+        print( "Number of lines above 12!" )
+        return 1
+    else:
+        print( "Number of linesOK!" )
+        return 0
