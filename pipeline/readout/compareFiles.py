7d41f20d883957d6221e9d74b1c4a51cede81225
Javier Galan <javier.galan@unizar.es>
2023-06-08 23:36:29 +0200

    updating compareFiles.py

--- pipeline/readout/compareFiles.py
+++ pipeline/readout/compareFiles.py
@@ -30,14 +30,14 @@ for line in file_1_text:
     numeric_filter_1 = filter(str.isdigit, line.rstrip())
     numeric_string_1 = "".join(numeric_filter_1)
 
-    if( numeric_string_1.find("132") == 0):
+    if( "[32m" in line):
         # removing color codes
         numeric_string_1 = numeric_string_1[3 : len(numeric_string_1) - 1]
 
     numeric_filter_2 = filter(str.isdigit, file_2_text[n].rstrip())
     numeric_string_2 = "".join(numeric_filter_2)
 
-    if( numeric_string_2.find("132") == 0):
+    if( "[32m" in file_2_text[n].rstrip() ):
         # removing color codes
         numeric_string_2 = numeric_string_2[3 : len(numeric_string_2) - 1]
 
