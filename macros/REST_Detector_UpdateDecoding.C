93a645ad8d33427d3b9306a18d879cc52bba62c7
juanan <juanan318@gmail.com>
2023-07-04 12:20:32 +0200

    Adding UpdateDecoding macro

---
+++ macros/REST_Detector_UpdateDecoding.C
@@ -0,0 +1,22 @@
+void REST_Detector_UpdateDecoding (const std::string& rootFile, const std::string &readoutName, const std::string & decodingFile ) {
+
+TFile *f = TFile::Open(rootFile.c_str(), "UPDATE");
+TRestDetectorReadout* readout = (TRestDetectorReadout*)f->Get(readoutName.c_str());
+
+//readout->PrintMetadata();
+
+  for( int p = 0; p < readout->GetNumberOfReadoutPlanes(); p++ ){
+    TRestDetectorReadoutPlane *plane = readout->GetReadoutPlane( p );
+      for( int m = 0; m < plane->GetNumberOfModules(); m++ ) {
+        TRestDetectorReadoutModule* module = &(*plane)[m];
+        module->SetDecodingFile(decodingFile);
+        module->UpdateDecoding();
+      }
+  }
+
+readout->Write(readoutName.c_str());
+
+f->Close();
+
+}
+
