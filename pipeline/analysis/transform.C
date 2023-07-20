22bb9d00e3a322a9014b7c8cab4101130df74a7f
Javier Galan <javier.galan@unizar.es>
2023-07-20 20:02:53 +0200

    Adding hitmap transformation example

---
+++ pipeline/analysis/transform.C
@@ -0,0 +1,26 @@
+
+Int_t transform(Bool_t draw = false) {
+    TRestDetectorHitsEvent* ev = new TRestDetectorHitsEvent();
+
+    Double_t xCenter = 5;
+    Double_t yCenter = 10;
+
+    TRandom3* random = new TRandom3(137);
+    for (int n = 0; n < 10000; n++) {
+        Double_t x = random->Gaus(xCenter, 3);
+        Double_t y = random->Gaus(yCenter, 3);
+
+        ev->AddHit(x, y, 0, 1);
+    }
+
+    // Initializing processes
+    string cfgFile1 = "transform.rml";
+    TRestDetectorHitsTransformationProcess* t =
+        new TRestDetectorHitsTransformationProcess((char*)cfgFile1.c_str());
+    t->PrintMetadata();
+
+    TRestDetectorHitsEvent* tEv = (TRestDetectorHitsEvent*)t->ProcessEvent(ev);
+
+    cout << "[\033[92m OK \x1b[0m]" << endl;
+    return 0;
+}
