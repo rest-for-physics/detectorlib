60370f3a1e865006540f5087f28bb038b3618ae5
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2023-08-30 15:20:08 +0200

    attempt to clean hit class (just a little bit)

--- src/TRestDetectorHitsEvent.cxx
+++ src/TRestDetectorHitsEvent.cxx
@@ -97,7 +97,7 @@ TRestDetectorHitsEvent::~TRestDetectorHitsEvent() { delete fHits; }
 /// structure. Additionally a time delay value in `us` may be added to the hits.
 void TRestDetectorHitsEvent::AddHit(Double_t x, Double_t y, Double_t z, Double_t en, Double_t t,
                                     REST_HitType type) {
-    fHits->AddHit(x, y, z, en, t, type);
+    fHits->AddHit({x, y, z}, en, t, type);
 }
 
 ///////////////////////////////////////////////
@@ -171,10 +171,12 @@ void TRestDetectorHitsEvent::Shuffle(int NLoop) {
 TRestHits* TRestDetectorHitsEvent::GetXZHits() {
     fXZHits->RemoveHits();
 
-    for (unsigned int i = 0; i < this->GetNumberOfHits(); i++)
-        if (GetType(i) == XZ)
-            fXZHits->AddHit(this->GetX(i), this->GetY(i), this->GetZ(i), this->GetEnergy(i), this->GetTime(i),
-                            XZ);
+    for (unsigned int i = 0; i < this->GetNumberOfHits(); i++) {
+        if (GetType(i) == XZ) {
+            fXZHits->AddHit({this->GetX(i), this->GetY(i), this->GetZ(i)}, this->GetEnergy(i),
+                            this->GetTime(i), XZ);
+        }
+    }
 
     return fXZHits;
 }
@@ -189,10 +191,12 @@ TRestHits* TRestDetectorHitsEvent::GetXZHits() {
 TRestHits* TRestDetectorHitsEvent::GetYZHits() {
     fYZHits->RemoveHits();
 
-    for (unsigned int i = 0; i < this->GetNumberOfHits(); i++)
-        if (GetType(i) == YZ)
-            fYZHits->AddHit(this->GetX(i), this->GetY(i), this->GetZ(i), this->GetEnergy(i), this->GetTime(i),
-                            YZ);
+    for (unsigned int i = 0; i < this->GetNumberOfHits(); i++) {
+        if (GetType(i) == YZ) {
+            fYZHits->AddHit({this->GetX(i), this->GetY(i), this->GetZ(i)}, this->GetEnergy(i),
+                            this->GetTime(i), YZ);
+        }
+    }
 
     return fYZHits;
 }
@@ -209,8 +213,10 @@ TRestHits* TRestDetectorHitsEvent::GetXYZHits() {
 
     for (unsigned int i = 0; i < this->GetNumberOfHits(); i++) {
         if (GetType(i) == XYZ) {
-            fXYZHits->AddHit(this->GetX(i), this->GetY(i), this->GetZ(i), this->GetEnergy(i),
-                             this->GetTime(i), XYZ);
+            {
+                fXYZHits->AddHit({this->GetX(i), this->GetY(i), this->GetZ(i)}, this->GetEnergy(i),
+                                 this->GetTime(i), XYZ);
+            }
         }
     }
 
